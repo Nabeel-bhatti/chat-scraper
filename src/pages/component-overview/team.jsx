29845c0bf0dffa4918837dac3f880a644dedd89c
Nabeel-bhatti <nabeelattari@gmail.com>
2025-04-29 16:32:43 +0500

    add delete alert dialoge in teams page

--- src/pages/component-overview/team.jsx
+++ src/pages/component-overview/team.jsx
@@ -1,20 +1,21 @@
 import {
-  Box,
-  Button,
-  styled,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  FormControl,
-  FormLabel,
-  TextField,
-  Autocomplete,
-  Stack,
-  IconButton,
-  Avatar,
-  ListItemAvatar,
-  Paper
+    Box,
+    Button,
+    styled,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    FormControl,
+    FormLabel,
+    TextField,
+    Autocomplete,
+    Stack,
+    IconButton,
+    Avatar,
+    ListItemAvatar,
+    Paper,
+    Typography,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
@@ -23,12 +24,14 @@ import { DeleteOutline, CloseOutlined } from '@mui/icons-material';
 // import * as yup from "yup";
 
 const team = () => {
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [rows, setRows] = useState([]);
-  const [paginationModel, setPaginationModel] = useState({
-    page: 0,
-    pageSize: 5
-  });
+
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialog2Open, setDialog2Open] = useState(false);
+    const [rows, setRows] = useState([]);
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: 5,
+    });
 
   useEffect(() => {
     setRows(
@@ -48,9 +51,13 @@ const team = () => {
     fontWeight: 500
   });
 
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-  };
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+    };
+    const handleDialog2Close = () => {
+        setDialog2Open(false);
+    };
+
 
   const columns = [
     // { field: "id", headerName: "Sr#", flex: 0.5, filterable: false },
@@ -106,154 +113,145 @@ const team = () => {
       flex: 2
     },
 
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      filterable: false,
-      renderCell: (params) => (
-        <IconButton
-          size="large"
-          sx={{
-            color: '#ef4444',
-            '&:hover': {
-              backgroundColor: '#ef44441a'
-            }
-          }}
-        >
-          <DeleteOutline fontSize="inherit" />
-        </IconButton>
-      )
-    }
-  ];
-  return (
-    <Box sx={{ p: { xs: 2, sm: 3 } }}>
-      <Box sx={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-        <Button
-          onClick={() => setDialogOpen(true)}
-          size='large'
-          sx={{
-            backgroundColor: '#2463EB',
-            color: '#fff',
-            fontSize: '0.875rem',
-            fontWeight: 500,
-            borderRadius: '6px',
-            px: '18px',
-            mb: 2,
-            '&:hover': { color: '#fff', backgroundColor: '#2463ebe6' }
-          }}
-        >
-          +&nbsp; Add Member
-        </Button>
-      </Box>
-      <DataGrid
-        columns={columns}
-        rows={rows}
-        paginationModel={paginationModel}
-        onPaginationModelChange={setPaginationModel}
-        columnHeaderHeight={37}
-        density="comfortable"
-        pageSizeOptions={[3, 5, 10]}
-        sx={{
-          border: '1px solid #e0e0e0',
-          mt: 1,
-          maxHeight: 380,
-          fontSize: '14px',
-          backgroundColor: '#fff',
-          '& .MuiDataGrid-row': {
-            borderBottom: '1px solid #d1d5db',
-            backgroundColor: '#fff',
-            fontSize: '14px',
-            color: '#09090b'
-          },
-          '& .MuiDataGrid-columnHeaders': {
-            borderBottom: '1px solid #e0e0e0'
-          },
-          '& .MuiDataGrid-columnHeader': {
-            // borderRight: "1px solid #f0f0f0",
-            fontSize: '12px',
-            color: '#71717a',
-            backgroundColor: '#fff'
-          }
-        }}
-      />
+        {
+            field: "actions",
+            headerName: "Actions",
+            filterable: false,
+            renderCell: (params) => (
 
-      <Dialog
-        open={dialogOpen}
-        onClose={handleDialogClose}
-        maxWidth={'xs'}
-        slotProps={{
-          backdrop: {
-            sx: {
-              backgroundColor: 'rgba(0, 0, 0, 0.8)'
-            }
-          }
-        }}
-      >
-        <DialogTitle
-          component={'h4'}
-          sx={{
-            paddingBottom: '0px',
-            color: '#09090b',
-            fontSize: '18px',
-            fontWeight: 600
-          }}
-        >
-          Add Team Member
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={() => setDialogOpen(false)}
-          sx={(theme) => ({
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            cursor: 'default',
-            color: theme.palette.grey[500],
-            '&:hover': {
-              backgroundColor: '#fff'
-            }
-          })}
-        >
-          <CloseOutlined
-            sx={{
-              height: '16px',
-              cursor: 'pointer',
-              width: '16px',
-              '&:hover': {
-                color: '#09090b',
-                backgroundColor: '#fff'
-              }
-            }}
-          />
-        </IconButton>
-        <DialogContent>
-          <FormControl sx={{ width: '100%' }}>
-            <Lable>Full Name *</Lable>
-            <TextField
-              size="small"
-              sx={{
-                mb: 3,
-                '& .MuiOutlinedInput-root': {
-                  borderRadius: '.4rem'
-                }
-              }}
-              variant="outlined"
-            />
-          </FormControl>
-          <FormControl sx={{ width: '100%' }}>
-            <Lable>Email *</Lable>
-            <TextField
-              size="small"
-              type="Email"
-              sx={{
-                mb: 3,
-                '& .MuiOutlinedInput-root': {
-                  borderRadius: '.4rem'
-                }
-              }}
-              variant="outlined"
-            />
-          </FormControl>
+                <IconButton
+                    size='large'
+                    onClick={() => setDialog2Open(true)}
+                    sx={{
+
+                        color: '#ef4444',
+                        '&:hover': {
+                            backgroundColor: "#ef44441a",
+
+                        }
+                    }}
+                >
+                    <DeleteOutline fontSize='inherit' />
+                </IconButton>
+
+
+
+            ),
+        },
+    ];
+    return (
+        <Box sx={{ p: { xs: 2, sm: 3, } }}>
+            <Paper
+                variant="outlined"
+                sx={{
+                    borderColor: '#e4e4e7',
+                    borderRadius: '7px',
+                    p: 2,
+                }} >
+                <Box sx={{ display: "flex", justifyContent: "end", alignItems: "center", }}>
+                    <Button
+                        onClick={() => setDialogOpen(true)}
+                        sx={{ backgroundColor: "#2463EB", color: "#fff", fontSize: "14px", fontWeight: 500, borderRadius: "6px", px: "18px", "&:hover": { color: "#fff", backgroundColor: "#2463ebe6" } }}
+                    >
+                        +&nbsp;  Add Member
+                    </Button>
+                </Box>
+                <DataGrid
+                    slots={{ toolbar: GridToolbar }}
+                    columns={columns}
+                    rows={rows}
+                    paginationModel={paginationModel}
+                    onPaginationModelChange={setPaginationModel}
+                    columnHeaderHeight={37}
+                    density='comfortable'
+                    pageSizeOptions={[3, 5, 10]}
+                    sx={{
+                        border: "1px solid #e0e0e0", mt: 1, maxHeight: 380, fontSize: "14px",
+                        "& .MuiDataGrid-row": {
+                            borderBottom: "1px solid #d1d5db",
+                            fontSize: "14px",
+                            color: "#09090b",
+                        },
+                        "& .MuiDataGrid-columnHeaders": {
+                            borderBottom: "1px solid #e0e0e0",
+
+                        },
+                        "& .MuiDataGrid-columnHeader": {
+                            // borderRight: "1px solid #f0f0f0",
+                            fontSize: "14px",
+                            color: "#71717a",
+                            backgroundColor: "#fff",
+
+
+                        },
+                    }}
+                />
+
+                <Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth={'xs'}
+                    slotProps={{
+                        backdrop: {
+                            sx: {
+                                backgroundColor: "rgba(0, 0, 0, 0.8)",
+                            },
+                        },
+                    }} >
+                    <DialogTitle component={"h4"} sx={{
+                        paddingBottom: "0px",
+                        color: "#09090b",
+                        fontSize: "18px",
+                        fontWeight: 600,
+                    }}>Add Team Member</DialogTitle>
+                    <IconButton
+                        aria-label="close"
+                        onClick={() => setDialogOpen(false)}
+                        sx={(theme) => ({
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            cursor: "default",
+                            color: theme.palette.grey[500],
+                            "&:hover": {
+                                backgroundColor: '#fff'
+                            }
+                        })}
+                    >
+                        <CloseOutlined sx={{
+                            height: "16px",
+                            cursor: "pointer",
+                            width: "16px", "&:hover": {
+                                color: "#09090b",
+                                backgroundColor: '#fff'
+                            }
+                        }} />
+
+                    </IconButton>
+                    <DialogContent>
+
+                        <FormControl sx={{ width: "100%" }}>
+                            <Lable >Full Name *</Lable>
+                            <TextField
+                                size="small"
+                                sx={{
+                                    mb: 3, '& .MuiOutlinedInput-root': {
+                                        borderRadius: '.4rem',
+                                    }
+                                }}
+                                variant="outlined"
+                            />
+                        </FormControl>
+                        <FormControl sx={{ width: "100%" }}>
+                            <Lable >Email *</Lable>
+                            <TextField
+                                size="small"
+                                type='Email'
+                                sx={{
+                                    mb: 3, '& .MuiOutlinedInput-root': {
+                                        borderRadius: '.4rem',
+                                    }
+                                }}
+                                variant="outlined"
+                            />
+                        </FormControl>
 
           <FormControl sx={{ width: '100%' }}>
             <Lable>Role *</Lable>
@@ -287,42 +285,66 @@ const team = () => {
           </FormControl>
         </DialogContent>
 
-        <DialogActions sx={{ pb: '24px', pr: '22px' }}>
-          <Button
-            onClick={() => setDialogOpen(false)}
-            color="primary"
-            sx={{
-              backgroundColor: '#fff',
-              color: '#09090b',
-              border: ' 1px solid #e4e4e7',
-              fontSize: '14px',
-              fontWeight: 500,
-              borderRadius: '6px',
-              px: '18px',
-              '&:hover': { color: '#2463EB', backgroundColor: '#ecf2fd' }
-            }}
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={() => setDialogOpen(false)}
-            color="primary"
-            sx={{
-              backgroundColor: '#2463EB',
-              color: '#fff',
-              fontSize: '14px',
-              fontWeight: 500,
-              borderRadius: '6px',
-              px: '18px',
-              '&:hover': { color: '#fff', backgroundColor: '#2463ebe6' }
-            }}
-          >
-            Add Member
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
-  );
-};
+                    <DialogActions sx={{ pb: "24px", pr: "22px" }}>
+                        <Button onClick={() => setDialogOpen(false)} color="primary" sx={{ backgroundColor: "#fff", color: "#09090b", border: " 1px solid #e4e4e7", fontSize: "14px", fontWeight: 500, borderRadius: "6px", px: "18px", "&:hover": { color: "#2463EB", backgroundColor: "#ecf2fd" } }}>
+                            Cancel
+                        </Button>
+                        <Button onClick={() => setDialogOpen(false)} color="primary" sx={{ backgroundColor: "#2463EB", color: "#fff", fontSize: "14px", fontWeight: 500, borderRadius: "6px", px: "18px", "&:hover": { color: "#fff", backgroundColor: "#2463ebe6" } }}>
+                            Add Member
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+                <Dialog open={dialog2Open} onClose={handleDialog2Close}
+                    slotProps={{
+                        backdrop: {
+                            sx: {
+                                pr: "24px",
+                                backgroundColor: "rgba(0, 0, 0, 0.8)",
+                            },
+                        },
+                    }} >
+                    <IconButton
+                        aria-label="close"
+                        onClick={() => setDialog2Open(false)}
+                        sx={(theme) => ({
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            cursor: "default",
+                            color: theme.palette.grey[500],
+                            "&:hover": {
+                                backgroundColor: '#fff'
+                            }
+                        })}
+                    >
+                        <CloseOutlined sx={{
+                            height: "16px",
+                            cursor: "pointer",
+                            width: "16px", "&:hover": {
+                                color: "#09090b",
+                                backgroundColor: '#fff'
+                            }
+                        }} />
+
+                    </IconButton>
+                    <DialogContent>
+                        <Typography sx={{
+                            fontSize: "18px", fontWeight: 600, color: "#09090b", mb: "8px"
+                        }}>Remove Team Member?</Typography>
+                        < Typography sx={{ fontSize: "14px", color: "#71717a", whiteSpace: "pre-line" }}>Are you sure you want to remove Jane Smith? This action cannot be<br /> undone. </Typography>
+
+
+                    </DialogContent>
+
+                    <DialogActions sx={{ pb: "24px", pr: "22px" }}>
+                        <Button onClick={() => setDialog2Open(false)} color="primary" sx={{ backgroundColor: "#fff", color: "#09090b", border: " 1px solid #e4e4e7", fontSize: "14px", fontWeight: 500, borderRadius: "6px", px: "18px", "&:hover": { color: "#2463EB", backgroundColor: "#ecf2fd" } }}>
+                            Cancel
+                        </Button>
+                        <Button onClick={() => setDialog2Open(false)} color="primary" sx={{ backgroundColor: "#ef4444", color: "#fff", fontSize: "14px", fontWeight: 500, borderRadius: "6px", px: "18px", "&:hover": { color: "#fff", backgroundColor: "#ef4444e6" } }}>
+                            Yes, Remove
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Paper>
 
 export default team;
